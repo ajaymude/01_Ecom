f26d50743b8ca1e1a145621f4d02d999376e5224
ajaymude <mudeajay@gmail.com>
2024-10-07 23:53:21 +0530

    token is added

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,5 +1,23 @@
 import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
+
 import User from "../models/user.model.js";
+
+export const generateTokens = (userId) => {
+	const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: "15m",
+	});
+
+	const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+		expiresIn: "7d",
+	});
+
+	return { accessToken, refreshToken };
+};
+
+export const storeRefreshToken = async (userId, refreshToken) => {
+	await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7days
+};
 
 export const protectRoute = async (req, res, next) => {
 	try {
@@ -38,4 +56,24 @@ export const adminRoute = (req, res, next) => {
 	} else {
 		return res.status(403).json({ message: "Access denied - Admin only" });
 	}
+};
+
+
+
+
+
+
+export const setCookies = (res, accessToken, refreshToken) => {
+	res.cookie("accessToken", accessToken, {
+		httpOnly: true, // prevent XSS attacks, cross site scripting attack
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		maxAge: 15 * 60 * 1000, // 15 minutes
+	});
+	res.cookie("refreshToken", refreshToken, {
+		httpOnly: true, // prevent XSS attacks, cross site scripting attack
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+	});
 };
